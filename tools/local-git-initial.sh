8bdfa3c7cc5868aaa39360830845aedd1ccafc4c
Eric Yang <yangyong.soloc@gmail.com>
2019-08-01 18:01:39 +0800

    add tool local-git-initial.sh

---
+++ tools/local-git-initial.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+NEW_REPO=repo-name
+if [ "$1" != "" ]
+then
+    NEW_REPO=$1
+fi
+
+# start sshd service
+# sudo systemctl start sshd.service
+# cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
+mkdir $NEW_REPO.git
+
+cd  $NEW_REPO.git
+
+git init --bare
+
+cd -
+
+mkdir test
+
+cd test
+
+git init .
+
+echo "# $NEW_REPO" > README.md
+
+git add .
+
+git commit -m "initial commit"
+
+git remote add origin localhost:`pwd`/../$NEW_REPO.git
+
+git push -u origin master
+
+cd -
+
+rm test -fr
